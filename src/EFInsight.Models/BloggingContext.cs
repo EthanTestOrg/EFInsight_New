7a02acea9bea9c332f4c9b9acfd9e73efacdef9b
ethanliuhp <min-ethan.liu@non.schneider-electric.com>
2016-12-19 11:31:06 +0800

    fdf

---
+++ src/EFInsight.Models/BloggingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFInsight.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ src/EFInsight.Models/Migrations/201612130526555_AddUserTable.cs
@@ -0,0 +1,26 @@
+namespace EFInsight.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddUserTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 128),
+                        UserDisplayName = c.String(),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
